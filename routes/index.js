b23aef958070fe9c519a737c7b4423525dfe44f3
TFlearyDev <tflearydev@gmail.com>
2019-02-28 15:57:04 -0500

    edits

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const shopController = require('../controllers/shopController.js')
+const foodController = require('../controllers/foodController.js')
 
+// Shop routes
 router.get('/', shopController.index)
 router.get('/new', shopController.new)
 router.get('/', shopController.create)
@@ -9,5 +11,14 @@ router.get('/:shopId', shopController.show)
 router.get('/:shopId/edit', shopController.edit)
 router.get('/:shopId', shopController.update)
 router.get('/:shopId', shopController.delete)
+
+
+// Food routes
+router.get('/:shopId/food/:foodId', foodController.show)
+router.get('/:shopId/food/new', foodController.new)
+router.post('/:shopId/food', foodController.create)
+router.get('/:shopId/food/:foodId/edit', foodController.edit)
+router.put('/:shopId/food/:foodId', foodController.update)
+router.delete('/:shopId/food/:foodId', foodController.delete)
 
 module.exports=router
