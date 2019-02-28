cb87bb07bde57ea51c556bb937f9ad63c8f3a2e0
Chad Christensen <chadchristensen2@gmail.com>
2019-02-28 01:13:50 -0500

    Heroku will work

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,22 @@
+// Require dotenv
+require('dotenv').config()
+
 // Require mongoose
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // Connect to a local database called welp
-mongoose.connect('mongodb://localhost/welp').then(() => {
-    console.log('Connected to MongoDB')
-})
-.catch((e) => {
-    console.log('An Error: ')
-    console.log(e)
-})
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect("mongodb://localhost/welp");
+}
+mongoose.connection.on("error", function(err) {
+  console.error("MongoDB connection error: " + err);
+  process.exit(-1);
+});
+mongoose.connection.once("open", function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 // export your mongoose connection
-module.exports = mongoose
+module.exports = mongoose;
