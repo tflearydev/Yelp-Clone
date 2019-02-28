cdf2d0facd50b1b0381dd76e7904082f63201dfc
TFlearyDev <tflearydev@gmail.com>
2019-02-28 16:05:57 -0500

    Many changes

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -1,7 +1,12 @@
+const Shop = require ()
+
 const foodController = {
     show: (req, res) => {
-        res.send(`Showing food item with id of ${req.params.foodId}`)
-    },
+        let{shopId, foodId}=req.params
+        Shop.findById(shopId).then(shop =>{
+        res.render('foods/show', {foundItem})
+    })
+},
     new: (req, res) => {
         res.send(`Show a form to create a new food item`)
     },
